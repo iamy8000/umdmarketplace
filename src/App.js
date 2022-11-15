778ad700fb9462c1f18b1b5785d30cac079a1cd3
iamy8000 <amy8000@gmail.com>
2022-11-15 13:08:26 -0500

    remove redirect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 /* Containers */
 import Navigation from 'components/navigation/Navigation';
 
 function App() {
-    useEffect(() => {
-        if (window.location.href !== `${window.origin}/umdmarketplace`) {
-            window.location.href = `${window.origin}/umdmarketplace`
-        }
-    }, [])
-
     return (
         <div>
             <Suspense fallback={<div />}>
-                <BrowserRouter basename="/umdmarketplace">
+                <BrowserRouter>
                     <Navigation />
                 </BrowserRouter>
             </Suspense>
